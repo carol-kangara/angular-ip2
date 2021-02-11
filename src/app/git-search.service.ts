75c693140c7ea91cd920dd7ff969a4fb9c622ed6
carol-kangara <carolkangara30@gmail.com>
2021-02-11 10:12:34 +0300

    add header

--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -16,6 +16,13 @@ export class GitSearchService {
    }
    getProfileInfo(){
      return this.http.get("https://api.github.com/users" + this.username + "?client_id=" + this.clientid + "&client_secret=" +this.cliensecret )
-     .map(res =>res.json());
+     .map(result =>result);
    }
+   getRepos() {
+    return this.http.get(' https://api.github.com/users/' + this.username + '/repos')
+    .map(result => result);
+}
+  updateUser(username: string) {
+  this.username = username;
+} 
 }
