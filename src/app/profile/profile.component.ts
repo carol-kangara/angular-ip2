468defc7d5cd2fc46db6b79eb22bd7843f4c335e
carol-kangara <carolkangara30@gmail.com>
2021-02-11 09:50:46 +0300

    add changes to component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {GithubService} from '../github.service';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  user: any= [];
+  repos: any= [];
+  username!: string;
+  constructor(private _githubService: GithubService) {
+    this._githubService.getUser().subscribe(user => {
+      console.log(user);
+      this.user = user;
+    });
+    this._githubService.getRepos().subscribe(repos => {
+      this.repos = repos;
+    });
+   }
 
   ngOnInit(): void {
+  }
+  searchUser() {
+    this._githubService.updateUser(this.username);
+    this._githubService.getUser().subscribe(user => {
+      this.user = user;
+    });
+    this._githubService.getRepos().subscribe(repos => {
+      this.repos = repos;
+    });
+
   }
 
 }
